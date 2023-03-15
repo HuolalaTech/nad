8ab66c2bf720d3efb304e4c915895ea83f06c2e3
YanagiEiichi <576398868@qq.com>
2023-03-15 11:49:06 +0800

    Remove package name string from code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import { NadInvoker } from './NadInvoker';
 import type { Settings } from './NadInvoker';
 
-if (typeof window === 'object') {
-  if ('NadInvoker' in window) {
-    // eslint-disable-next-line no-console
-    console.error('"@huolala-tech/nad-runtime" installed duplicately with different versions.');
+// Find the globalThis object across browsers and miniprogram platforms.
+const globalThis =
+  typeof window === 'object' ? window : typeof global === 'object' ? global : /* istanbul ignore next */ null;
+if (globalThis) {
+  // This key is the MD5 hash of the package name.
+  const key = 'e7de5b84066fc28a0206fbac19447bc2';
+  if (key in globalThis) {
+    // Throw an error if this key was set in the global object.
+    console.error(new Error(`The "nad-runtime" lib was installed duplicately with different versions.`));
+  } else {
+    // Set the key to the global object.
+    Object.defineProperty(globalThis, key, {
+      configurable: true,
+      value: NadInvoker,
+    });
   }
-  Object.defineProperty(window, 'NadInvoker', { configurable: true, value: NadInvoker });
 }
 
 export { NadInvoker };
