bba39369c19329a67632d7d7e03180cff73bf51b
YanagiEiichi <576398868@qq.com>
2023-04-24 18:20:40 +0800

    Update comments

--- src/main/java/cn/lalaframework/nad/NadCore.java
+++ src/main/java/cn/lalaframework/nad/NadCore.java
@@ -15,13 +15,23 @@ public class NadCore {
     @Autowired(required = false)
     private RequestMappingHandlerMapping handlerMapping;
 
+    /**
+     * Find all routes from Spring Web and create a NadResult.
+     * @return A NadResult object.
+     */
     @NonNull
     public NadResult create() {
         return create(null);
     }
 
+    /**
+     * Find all routes from Spring Web and create a NadResult.
+     * @param classExcluder An implementation of the ClassFilter interface to specify which classes should be excluded.
+     *                      You can also use the util implementation of cn.lalaframework.nad.utils.ClassExcluder.
+     * @return A NadResult object.
+     */
     @NonNull
-    public NadResult create(ClassFilter classFilter) {
+    public NadResult create(ClassFilter classExcluder) {
         if (handlerMapping == null) throw new NoHandlerMappingException();
         return NadContext.run(() -> {
             List<NadRoute> routes = NadRoute.fromMapping(handlerMapping);
@@ -29,7 +39,7 @@ public class NadCore {
             List<NadClass> classes = NadContext.dumpClasses();
             List<NadEnum> enums = NadContext.dumpEnums();
             return new NadResult(modules, routes, classes, enums);
-        }, classFilter);
+        }, classExcluder);
     }
 }
 
