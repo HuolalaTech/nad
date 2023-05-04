38734cbdec3e1507674160311befd9ba96ed9fad
YanagiEiichi <576398868@qq.com>
2023-05-04 11:19:10 +0800

    Add isNegated field for NameValuePair

--- src/main/java/cn/lalaframework/nad/models/NameValuePair.java
+++ src/main/java/cn/lalaframework/nad/models/NameValuePair.java
@@ -4,19 +4,18 @@ import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.mvc.condition.NameValueExpression;
 
-public class NameValuePair {
+public class NameValuePair implements NameValueExpression<String> {
     @NonNull
     private final String name;
     @Nullable
     private final String value;
 
-    public NameValuePair(NameValueExpression<String> expression) {
-        this(expression.getName(), expression.getValue());
-    }
+    private final boolean isNegated;
 
-    public NameValuePair(@NonNull String name, String value) {
-        this.name = name;
-        this.value = value;
+    public NameValuePair(@NonNull NameValueExpression<String> expression) {
+        this.name = expression.getName();
+        this.value = expression.getValue();
+        this.isNegated = expression.isNegated();
     }
 
     @NonNull
@@ -27,5 +26,10 @@ public class NameValuePair {
     @Nullable
     public String getValue() {
         return value;
+    }
+
+    @Override
+    public boolean isNegated() {
+        return this.isNegated;
     }
 }
