f9d3a7dbcf019bad3d473e0dec2b18408295998d
YanagiEiichi <576398868@qq.com>
2023-07-05 13:52:17 +0800

    Update NadResourceResolver

--- nad-java-sdk/src/main/java/cn/lalaframework/nad/NadResourceResolver.java
+++ nad-java-sdk/src/main/java/cn/lalaframework/nad/NadResourceResolver.java
@@ -1,11 +1,12 @@
 package cn.lalaframework.nad;
 
 import org.springframework.core.io.Resource;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 public class NadResourceResolver extends PathResourceResolver {
     @Override
-    protected Resource getResource(String resourcePath, Resource location) {
-        return location.isReadable() ? location : null;
+    protected Resource getResource(@NonNull String resourcePath, @NonNull Resource location) {
+        return location;
     }
 }
